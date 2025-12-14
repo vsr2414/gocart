24e42da2ac8d6fb09f32e2e3327ff1543bd7e805
vsr2414 <visitha2414@gmail.com>
2025-12-14 22:39:02 +0530

    modified:   app/api/store/product/route.js

--- app/api/store/product/route.js
+++ app/api/store/product/route.js
@@ -1,6 +1,8 @@
 import {getAuth} from "@clerk/nextjs/server"
 import authSeller from "@/middlewares/authSeller"
 import imagekit from "@/configs/imageKit"
+import prisma from "@/lib/prisma"
+import { NextResponse } from "next/server"
 
 //Add a new product
 export async function POST(request) {
@@ -27,10 +29,39 @@ export async function POST(request) {
 
         //upload images to imagekit
         const imagesUrl = await Promise.all(images.map(async (image) => {
-            const buffer = Buffer.from(await image.arrayBuffer()) 
+            const buffer = Buffer.from(await image.arrayBuffer());
+            const response= await imagekit.upload({
+                file: buffer,
+                fileName: image.name,
+                folder: "products",
+            }) 
+            const url = imagekit.url({
+                path: response.filePath,
+                transformation: [
+                    {quality: 'auto'},
+                    {format: 'webp'},
+                    {width: '1024'}
+                ]
+            })
+            return url
         }))
+        await prisma.product.create({
+            data: {
+                name,
+                description,
+                mrp,
+                price,
+                category,
+                images: imagesUrl,
+                storeId
+
+            }
+        })
+        return NextResponse.json({message: "Product added successfully"})
 
     } catch (error) {
+        console.error(error);
+        return NextResponse.json({error})
 
     }
 }
