8f4ea24292f6e354356bc3350a8fef1b88ce7e82
vsr2414 <visitha2414@gmail.com>
2025-12-22 20:29:52 +0530

    modified:   app/admin/layout.jsx 	modified:   components/admin/AdminLayout.jsx

--- app/admin/layout.jsx
+++ app/admin/layout.jsx
@@ -10,9 +10,16 @@ export default function RootAdminLayout({ children }) {
 
     return (
         <>
+            <SignedIn>
             <AdminLayout>
                 {children}
             </AdminLayout>
+            </SignedIn>
+            <SignedOut>
+                <div className = "min-h-screen flex items-center justify-center">
+                <SignIn fallbackRedirectUrl="/admin" routing="hash" />
+                </div>
+            </SignedOut>
         </>
     );
 }
