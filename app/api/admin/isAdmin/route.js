d546c3d70419d247149f3d565d33bfd65b6cc6d1
vsr2414 <visitha2414@gmail.com>
2025-12-23 21:44:00 +0530

    modified:   app/api/admin/isAdmin/route.js

--- app/api/admin/isAdmin/route.js
+++ app/api/admin/isAdmin/route.js
@@ -1,6 +1,8 @@
-import { getAuth } from "@clerk/nextjs/dist/types/server";
+import { getAuth } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 import authAdmin from "@/middlewares/authAdmin";
+import { prisma } from "@/lib/prisma";
+
 
 //Auth Admin
 export async function GET(request) {
