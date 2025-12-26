94df87bae8dd0692c18b5837559f8b384bd6ed65
vsr2414 <visitha2414@gmail.com>
2025-12-26 11:57:40 +0530

    modified:   app/(public)/layout.jsx 	modified:   lib/features/address/addressSlice.js 	modified:   lib/features/cart/cartSlice.js

--- app/(public)/layout.jsx
+++ app/(public)/layout.jsx
@@ -2,8 +2,38 @@
 import Banner from "@/components/Banner";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { fetchProducts } from "@/lib/features/product/productSlice";
+import { useSelector } from "react-redux";
+import { fetchCart, uploadCart } from "@/lib/features/cart/cartSlice";
+import { useUser, useAuth } from "@clerk/nextjs";
 
 export default function PublicLayout({ children }) {
+
+        const dispatch = useDispatch();
+        const {user} = useUser()
+        const {getToken} = useAuth()
+
+        const {cartItems} = useSelector((state) => state.cart)
+
+    useEffect(() => {
+        dispatch(fetchProducts({}));
+    }, [])
+
+    useEffect(() => {
+        if (user) {
+            dispatch(fetchCart({getToken}))
+            dispatch(fetchAddress({getToken}))
+        }
+    }, [user])
+
+    useEffect(() => {
+        if (user) {
+            dispatch(uploadCart({getToken}))
+        }
+    }, [cartItems])
+
 
     return (
         <>
