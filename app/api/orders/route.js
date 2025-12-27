4e1c0423157e9cd05840455a044d201f9b7d5fd8
vsr2414 <visitha2414@gmail.com>
2025-12-28 02:57:17 +0530

    modified:   app/api/orders/route.js 	modified:   package-lock.json 	modified:   package.json

--- app/api/orders/route.js
+++ app/api/orders/route.js
@@ -3,6 +3,9 @@ import { NextResponse } from "next/server";
 import { getAuth } from "@clerk/nextjs/server";
 import prisma from "@/lib/prisma";
 import { PaymentMethod } from "@prisma/client";
+import Stripe from "stripe";
+import { metadata } from "@/app/layout";
+
 
 
 export async function POST(request) {
@@ -104,6 +107,35 @@ export async function POST(request) {
                     })
                     orderIds.push(order.id)
                 }
+
+                if (paymentMethod === 'STRIPE') {
+                    const stripe = Stripe(process.env.STRIPE_SECRET_KEY)
+                    const origin = await request.headers.get('origin')
+
+                    const session = await stripe.checkout.sessions.create({
+                        payment_method_types: ['card'],
+                        line_items: [{
+                            price_data: {
+                                currency: 'usd',
+                                product_data: {
+                                    name: 'Order'
+                                },
+                                unit_amount: Math.round(fullAmount * 100)
+                            },
+                            quantity: 1
+                        }],
+                        expires_at: Math.floor(Date.now() / 1000) + 30 * 60, //current time + 30 minutes
+                        mode: 'payment',
+                            success_url: `${origin}/loading?nextUrl=orders`,
+                            cancel_url: `${origin}/cart`,
+                            metadata: { orderIds: orderIds.join(','),
+                                userId,
+                                appId: 'gocart'
+                            }
+                    })
+                    return NextResponse.json({ session})
+                }
+
 
                 //clear the cart 
                 await prisma.user.update({
